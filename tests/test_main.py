a06f396403a04109244412191c5a0f7e028b8b87
Shahul Hameed <shahulhameed181224@gmail.com>
2026-06-22 10:16:50 +0530

    Updated test main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from fastapi.testclient import TestClient
+'''from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
@@ -6,4 +6,18 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
 
+    assert response.status_code == 200'''
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
     assert response.status_code == 200
